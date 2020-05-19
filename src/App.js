5331ef4557732289e4d9fe502fb41a3fb8508540
Peter <peter.gorzo@posteo.net>
2020-05-19 20:48:41 +0200

    Easier testing

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+const colors = ['black', 'red', 'blue', 'green']
+
 function App() {
+  const [numResizes, setNumResizes] = useState(0)
   const handleResize = () => {
     console.log('Resize Event Triggered with following dimensions:')
     console.log({ width: window.innerWidth, height: window.innerHeight })
+    setNumResizes(numResizes + 1)
   }
 
   useEffect(() => {
+    window.removeEventListener('resize', handleResize)
     window.addEventListener('resize', handleResize)
-  }, [])
+  }, [numResizes])
 
   return (
-    <div className="App">
+    <div className="App"
+      style={{
+        background: colors[numResizes % 4]
+      }}
+    >
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          1. Connect to iOS device via Safari Developer Menu.
+          1. Open this site with an iOS device.
         </p>
         <p>
-          2. Observe console outputs.
+          2. Pinch zoom substantially.
         </p>
         <p>
-          3. Pinch Zoom substantially
-        </p>
-        <p>
-          4. Is width and height updated?
+          3. If the background color changes window resize events are getting triggered.
         </p>
       </header>
     </div>
